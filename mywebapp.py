5dd6c2ee61566eeb71d0dddca6269235630e3ab7
Zheni Nikolaeva Dimitrova <zhen0101@stud.kea.dk>
2019-03-14 16:56:37 +0100

    changes

--- mywebapp.py
+++ mywebapp.py
@@ -2,21 +2,21 @@ from collections import Counter
 import numpy as np
 import re
 import pickle
-import os
+import os #imports
 from flask import Flask, request, session, g, redirect, url_for, abort, \
      render_template, flash, jsonify
-app = Flask(__name__, template_folder='./templates')
+app = Flask(__name__, template_folder='./templates') #initialize the flask - used to create web apps with python- and we set the templates foldet for the template of the html
 
 label = {0:'ham', 1:'spam'}
 stop = pickle.load(open(
                 os.path.join('model', 
                 'pickles', 
-                'stopwords.pkl'), 'rb'))
+                'stopwords.pkl'), 'rb')) # loading the stopwords
 
 classifier = pickle.load(open(
                 os.path.join('model', 
                 'pickles', 
-                'classifier.pkl'), 'rb'))
+                'classifier.pkl'), 'rb')) # loading the classifier
 
 def make_Dictionary(train_dir):
     emails = [os.path.join(train_dir,f) for f in os.listdir(train_dir)]    
@@ -26,7 +26,7 @@ def make_Dictionary(train_dir):
             for i,line in enumerate(m):
                 if i == 2:  #Body of email is only 3rd line of text file
                     words = line.split()
-                    all_words += words
+                    all_words += words 
     
     dictionary = Counter(all_words)
     list_to_remove = dictionary.keys()
@@ -36,7 +36,7 @@ def make_Dictionary(train_dir):
         elif len(item) == 1:
             del dictionary[item]
     dictionary = dictionary.most_common(3000)
-    return dictionary
+    return dictionary #same function to create a dictionary - we put it here in order for pythn to be able to use it 
 
 
 def extract_features(mail_dir): 
@@ -57,7 +57,7 @@ def extract_features(mail_dir):
                   wordID = i
                   features_matrix[docID,wordID] = words.count(word)
         docID = docID + 1     
-    return features_matrix
+    return features_matrix # same extract feature because this is a python file
 
 
 
@@ -66,17 +66,17 @@ def extract_features(mail_dir):
  
 @app.route("/")
 def index():
-  return render_template('index.html')
+  return render_template('index.html') # set the route
 
 def write_mail_to_file(mailContent = ''):
   path = './data/example'
   filename = 'example.txt'
   if not os.path.exists(path):
-      os.makedirs(path)
+      os.makedirs(path) 
 
   f = open(os.path.join(path, filename), 'w+')
   f.write(mailContent)
-  f.close()
+  f.close() #function tp save the mail tht we he have inputted to save it in the file exapmle . txt in the folder data/example
 
 @app.route("/check-email", methods=['POST'])
 def check_mail():
@@ -93,8 +93,8 @@ def check_mail():
   except Exception as e:
         print(e)
         res = {"success": False, "error": e}
-        return jsonify(res)
+        return jsonify(res)  # post method to make the request based on what you have and return result wether its a spam or not and return the probabilit of it being true
 
 
 if __name__ == "__main__":
-  app.run(port = 5002, debug=True)
+  app.run(port = 5002, debug=True) # this is for flask so it knows on which port python flask app should run the code
